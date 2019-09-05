ab1b0e85ea8a78f388ec9c232ac1448a14c4f230
Danny Jackson <dnjacks4@asu.edu>
2019-09-05 14:39:56 -0700

    Update subset_ABBABABAwindows_output.py

--- subset_ABBABABAwindows_output.py
+++ subset_ABBABABAwindows_output.py
@@ -1,16 +1,14 @@
 import sys
+import csv
 
 file = sys.argv[1]
 
 outputList = []
 
-with open(file) as f:
+with open(file) as csv_file:
     line = f.readline()
     while line:
         fdstat = line.split(",")[10]
         if fdstat > 0.3:
-            outputList.append(line)
-            outputList.append("\n")
+            outputList.writerow(line)
         line = f.readline()
-
-print outputList
