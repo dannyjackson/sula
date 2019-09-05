35bd6f2b67f3ad055e0fb7192d54f34d3887f02f
Danny Jackson <dnjacks4@asu.edu>
2019-09-05 08:44:15 -0700

    works in progress

--- ABBAsliding.r
+++ ABBAsliding.r
@@ -5,7 +5,51 @@
 
 args = commandArgs()
 
+inputfile = substr(args[grep("inputfile_", args)],11,100000)
 outputdirectory = substr(args[grep("outputdirectory_", args)],17,100000)
-population1 = substr(args[grep("population1_", args)],13,100000)
-population2 = substr(args[grep("population2_", args)],13,100000)
-population3 = substr(args[grep("population3_", args)],13,100000)
+P1 = substr(args[grep("population1_", args)],13,100000)
+P2 = substr(args[grep("population2_", args)],13,100000)
+P3 = substr(args[grep("population3_", args)],13,100000)
+
+
+D.stat <- function(p1, p2, p3) {
+    ABBA <- (1 - p1) * p2 * p3
+    BABA <- p1 * (1 - p2) * p3
+    (sum(ABBA) - sum(BABA)) / (sum(ABBA) + sum(BABA))
+    }
+
+freq_table = read.table("$inputfile", header=T, as.is=T)
+
+#check for buried NAs
+for (i in 1:length(freq_table[1,])){
+  print(anyNA(freq_table[,i]))
+}
+
+#removing these values
+freq_table2<-freq_table
+freq_table<-na.omit(freq_table2)
+
+D <- D.stat(freq_table[,P1], freq_table[,P2], freq_table[,P3])
+
+print(paste("D =", round(D,4)))
+
+source("~/genomics_general-master/jackknife.R")
+
+block_indices <- get_block_indices(block_size=1e6,
+                                   positions=freq_table$position,
+                                   chromosomes=freq_table$scaffold)
+
+n_blocks <- length(block_indices)
+
+print(paste("Genome divided into", n_blocks, "blocks."))
+
+D_sd <- get_jackknife_sd(block_indices=block_indices,
+                         FUN=D.stat,
+                         freq_table[,P1], freq_table[,P2], freq_table[,P3])
+
+print(paste("D standard deviation = ", round(D_sd,4)))
+
+D_err <- D_sd/sqrt(n_blocks)
+D_Z <- D / D_err
+
+print(paste("D Z score = ", round(D_Z,3)))
