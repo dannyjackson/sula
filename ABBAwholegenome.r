13d9b6c30fc677b5a991a12db9de551d492d7c52
Danny Jackson <dnjacks4@asu.edu>
2019-09-12 13:31:58 -0700

    works in progress

--- ABBAwholegenome.r
+++ ABBAwholegenome.r
@@ -8,6 +8,7 @@ args = commandArgs()
 inputfile = substr(args[grep("inputfile_", args)],11,100000)
 outputdirectory = substr(args[grep("outputdirectory_", args)],17,100000)
 simonhmartin_directory = substr(args[grep("simonhmartin_directory_", args)],24,100000)
+project_name = substr(args[grep("project_name_", args)],14,100000)
 P1 = paste0(substr(args[grep("population1_", args)],13,100000))
 P2 = paste0(substr(args[grep("population2_", args)],13,100000))
 P3 = paste0(substr(args[grep("population3_", args)],13,100000))
@@ -22,20 +23,13 @@ D.stat <- function(p1, p2, p3) {
 
 freq_table = read.table(paste0(inputfile), header=T, as.is=T)
 
-#check for buried NAs
-for (i in 1:length(freq_table[1,])){
-  print(anyNA(freq_table[,i]))
-}
-
-#removing these values
+#removing NA values
 freq_table2<-freq_table
 freq_table<-na.omit(freq_table2)
 
 D <- D.stat(freq_table[,P1], freq_table[,P2], freq_table[,P3])
 
-#print_this_1 <- print(paste("D =", round(D,4)))
-#write(print_this_1, file = paste0(outputdirectory,".abbawholegenome.stats.txt") append = FALSE)
-cat(paste("D =", round(D,4)),file=paste0(outputdirectory,".abbawholegenome.stats.txt",sep="\n",append=TRUE))
+cat(paste("D =", round(D,4)),file=paste0(outputdirectory,project_name,"abbawholegenome.stats.txt",sep="\n",append=TRUE))
 print("this?")
 source(paste0(simonhmartin_directory,"/jackknife.R"))
 
@@ -45,22 +39,15 @@ block_indices <- get_block_indices(block_size=1e6,
 
 n_blocks <- length(block_indices)
 
-#print_this_2 <- print(paste("Genome divided into", n_blocks, "blocks."))
-#write(print_this_2, file = paste0(outputdirectory,".abbawholegenome.stats.txt") append = TRUE)
-cat(paste("Genome divided into", n_blocks, "blocks."),file=paste0(outputdirectory,".abbawholegenome.stats.txt",sep="\n",append=TRUE))
+cat(paste("Genome divided into", n_blocks, "blocks."),file=paste0(outputdirectory,project_name,".abbawholegenome.stats.txt",sep="\n",append=TRUE))
 
 D_sd <- get_jackknife_sd(block_indices=block_indices,
                          FUN=D.stat,
                          freq_table[,P1], freq_table[,P2], freq_table[,P3])
 
-#print_this_3 <- print(paste("D standard deviation = ", round(D_sd,4)))
-#write(print_this_3, file = paste0(outputdirectory,".abbawholegenome.stats.txt") append = TRUE)
-cat(paste("D standard deviation = ", round(D_sd,4)),file=paste0(outputdirectory,".abbawholegenome.stats.txt",sep="\n",append=TRUE))
+cat(paste("D standard deviation = ", round(D_sd,4)),file=paste0(outputdirectory,project_name,".abbawholegenome.stats.txt",sep="\n",append=TRUE))
 
 D_err <- D_sd/sqrt(n_blocks)
 D_Z <- D / D_err
 
-#print_this_4 <- print(paste("D Z score = ", round(D_Z,3)))
-#write(print_this_4, file = paste0(outputdirectory,".abbawholegenome.stats.txt", append = TRUE)
-
-cat(paste("D Z score = ", round(D_Z,3)),file=paste0(outputdirectory,".abbawholegenome.stats.txt",sep="\n",append=TRUE))
+cat(paste("D Z score = ", round(D_Z,3)),file=paste0(outputdirectory,project_name,".abbawholegenome.stats.txt",sep="\n",append=TRUE))
