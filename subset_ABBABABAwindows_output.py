0192b94f47725572f6db561b6c1ebb79d088ac90
Danny Jackson <dnjacks4@asu.edu>
2019-10-24 09:58:29 -0700

    Update subset_ABBABABAwindows_output.py

--- subset_ABBABABAwindows_output.py
+++ subset_ABBABABAwindows_output.py
@@ -10,14 +10,6 @@ with open(file) as f:
     line = f.readline()
     while line:
         fdstat = line.split(",")[9]
-        if fdstat > threshold:
-            outputList.write(line)
-        line = f.readline()
-
-with open(file) as f:
-    line = f.readline()
-    while line:
-        fdstat = line.split(",")[9]
         if ( fdstat == "fd" ):
             outputList.write(line)
         if ( fdstat != "fd" ):
