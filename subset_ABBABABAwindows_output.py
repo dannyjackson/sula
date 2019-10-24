037d1131a7157e87d67de92fe136a1306a35b0de
Danny Jackson <dnjacks4@asu.edu>
2019-10-24 10:19:29 -0700

    Update subset_ABBABABAwindows_output.py

--- subset_ABBABABAwindows_output.py
+++ subset_ABBABABAwindows_output.py
@@ -6,12 +6,15 @@ threshold = sys.argv[2]
 
 outputList = open(project_name, "a")
 
-    with open(file) as f:
+with open(file) as f:
+    line = f.readline()
+    while line:
+        fdstat = line.split(",")[1]
+        if ( fdstat == "fd" ):
+            outputList.write(line)
+        if ( fdstat != "fd" ):
+          if ( float(fdstat) > float(threshold) ):
+              outputList.write(line)
         line = f.readline()
-        while line:
-            fdstat = line.split(",")[0]
-            if fdstat > threshold:
-                outputList.write(line)
-            line = f.readline()
 
 outputList.close()
