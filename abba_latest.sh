e641ed362f8e18f0c67cad4c898833a1406a9e8d
Danny Jackson <dnjacks4@asu.edu>
2019-10-24 11:16:46 -0700

    Update abba_latest.sh

--- abba_latest.sh
+++ abba_latest.sh
@@ -1,49 +1,10 @@
 #!/bin/bash
 
-#A shell script to perform sliding window ABBA BABA tests on whole genome SNP data, with an option to output a list of candidate genes from high regions of introgression. Uses scripts developed by Simon Martin to perform sliding window analyses.
-
-#Program stages are as follows. The first stage is required, but each stage after that is optional. This means that stage 1 and 2 can be performed, or stage 1 and 3, or simply stage 1.
-
-#0) Parses the vcf file into a geno file. Can be skipped by providing the geno file instead of the vcf file.
-#1)Performs a genome wide ABBA BABA test. Outputs a block jackknife procedure to estimate standard deviation for D statistic. Outputs the D statistic, the D standard deviation, and the Z score in a text file with the suffix _genomewide_abbababa_stats.txt
-#2) Perform sliding window ABBA / BABA analyses that look for regions of introgression between population 2 and population 3 (most strongly sensitive to introgression from P3 -> P2)
-#3) Plot as a pdf the points at which introgression has been detected (note that if the input data is in scaffolds of relatively small size, this feature is not very meaningful as the scaffolds are not ordered in a biologically relevant way)
-#4) Requires that the reference genome used to generate the VCF file (hereafter refered to as the primary reference genome) does not have its own gff file but instead has been aligned to another genome using the SatsumaSynteny program (hereafter referred to as the secondary reference genome). This stage first identifies windows in which high introgression has been identified, and then identifies regions of the secondary reference genome where the regions of high introgression from the primary reference genome have aligned to. Finally, it looks at the gff file and pulls the names of genes that are within the regions of the secondary genome which have been identified as candidate regions of introgression. This list can easily be input into a gene ontology program, such as DAVID.
-
-#Inputs required for each stage:
-
-#0) Administrative inputs. A name for the project, a location of the directory for the output files of this program, the location of the directory into which the supporting executables have been put (just those from the dannyjackson github), and the location of the directory into which the supporting executables of Simon Martin's github have been put (https://github.com/simonhmartin/genomics_general).
-
-#1) A VCF file, a file assigning individuals (column 1) to populations (column 2), and a defined outgroup
-
-#2) The only thing necessary here is the go ahead from the user to proceed to this step.
-
-#3) The only thing necessary here is the go ahead from the user to proceed to this step.
-
-#4) Here we need two files, one is an output from Satsuma titled satsuma_summary.chained.out and the other is a path to the directory containing the gff files of the genome to which Satsuma aligned the primary reference genome. These should each be labelled as "secondaryreference_chromosome1.gff" "secondaryreference_chromosome2.gff" etc. using chromosome numbers that correspond with those used in the satsuma_summary_chained.out file.
+#A shell script to perform sliding window ABBA BABA tests on whole genome SNP data
 
 if [ $# -lt 1 ]
   then
-    echo "Pulls list of lines from chicken gff file that Satsuma aligned to regions of high introgression, as identified with sliding ABBA BABA windows
-
-    #administrative inputs
-    [-a] Name of project; name that all output files will be prefixed with
-    [-b] Output directory for files. Must be empty.
-    [-c] Path to directory containing all supporing files from github
-    [-d] Path to Simon Martin's genomics_general-master directory
-
-    #stage 1
-    [-1a] Complete path to input vcf or vcf.gz file, or to .geno or geno.gz file.
-    [-1b] File with two tab separated columns. Column 1 has sample names as they appear in the vcf, and column 2 has the population names to which each sample will be attributed. Populations should be listed together, not with some individuals from Pop1 followed by Pop2 followed by more Pop1. IMPORTANTLY the last population will be treated as your outgroup in the ABBA BABA analysis, or your P4, so be sure to list it last.
-
-    #stage 2
-
-    #stage 3
-    [-2a] write "y" if you want a plot, write "n" or leave empty if you don't
-
-    #stage 4
-    [-3a] Path to satsuma_summary_chained.out file
-    [-3b] Path to directory containing gff files"
+    echo "I still have to write an introduction to this script."
 
   else
     while getopts a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:p: option
