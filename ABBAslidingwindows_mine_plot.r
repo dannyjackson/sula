5080049170a40b9b7c308a649b9426c412a32686
Danny Jackson <dnjacks4@asu.edu>
2019-10-24 17:57:39 -0700

    Update ABBAslidingwindows_mine_plot.r

--- ABBAslidingwindows_mine_plot.r
+++ ABBAslidingwindows_mine_plot.r
@@ -11,5 +11,5 @@ outputdirectory = substr(args[grep("outputdirectory_", args)],17,100000)
 data <- read.csv(paste0(outputdirectory,"/",project_name,"_slidingwindows.csv.gz"))
 
 pdf(file = paste0(outputdirectory,"/",project_name,".slidingwindow_fdstats_plot.pdf"), width = 20, height = 7, useDingbats=FALSE)
-hist(data$fd, xlim=c(0,1), breaks =   c(-1000,0,0.05,0.1,0.15,0.20,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1,1000), ylim=c(0,1))
+hist(data$fd, xlim=c(0,1), breaks =   c(-1000,0,0.05,0.1,0.15,0.20,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1,1000), ylim=c(0,3))
   dev.off()
