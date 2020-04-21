d13cb88e406eba3ead7708f72e68bd9427e0f5e5
dannyjackson <49250788+dannyjackson@users.noreply.github.com>
2020-04-21 16:33:01 -0700

    Update pcadapt.R

--- pcadapt.R
+++ pcadapt.R
@@ -37,7 +37,7 @@ pdf(file = "hist.pdf", width = 20, height = 10, useDingbats=FALSE)
 sumx <- summary(x)
 cat(paste(sumx),file=paste0("pcadapt_summary.txt"),sep="\n",append=TRUE)
 
-q-values
+#q-values
 library(qvalue)
 qval <- qvalue(x$pvalues)$qvalues
 alpha <- 0.1
@@ -49,7 +49,7 @@ cat(paste("Length of q-value outliers= ", lo_q),file=paste0("pcadapt_stats.txt")
 cat(paste(outliers_q),file=paste0("outliers_q.txt"),sep="\n",append=TRUE)
 
 
-Benjamini-Hochberg Procedure
+#Benjamini-Hochberg Procedure
 padj <- p.adjust(x$pvalues,method="BH")
 alpha <- 0.1
 outliers_bh <- which(padj < alpha)
@@ -58,7 +58,7 @@ lo_bh <- length(outliers_bh)
 cat(paste("Length of Benjamini-Hochberg outliers= ", lo_bh),file=paste0("pcadapt_stats.txt"),sep="\n",append=TRUE)
 cat(paste(outliers_bh),file=paste0("outliers_bh.txt"),sep="\n",append=TRUE)
 
-Bonferroni correction
+#Bonferroni correction
 padj <- p.adjust(x$pvalues,method="bonferroni")
 alpha <- 0.1
 outliers_bf <- which(padj < alpha)
