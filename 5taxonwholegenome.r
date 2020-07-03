795b593ba793318ae662775cd73b1424d7bad3fd
Danny Jackson <dnjacks4@asu.edu>
2020-07-03 15:53:08 -0600

    Update 5taxonwholegenome.r

--- 5taxonwholegenome.r
+++ 5taxonwholegenome.r
@@ -4,7 +4,7 @@ Dfo.stat <- function(p1, p2, p3, p4) {
 
     BABAA <- p1 * (1 - p2) * p3 * (1 - p4)
     BBBAA <- p1 * p2 * p3 * (1 - p4)
-    ABABA <- (1 - p1) * p2 * (1-p3) * p4
+    ABABA <- (1 - p1) * p2 * (1 - p3) * p4
     AAABA <- (1 - p1) * (1 - p2) * (1 - p3) * p4
 
     BAABA <- p1 * (1 - p2) * (1 - p3) * p4
@@ -20,15 +20,15 @@ Dfo.stat <- function(p1, p2, p3, p4) {
 
 Dil.stat <- function(p1, p2, p3, p4) {
 
-    ABBAA <- (1 - p1) + p2 + p3 + (1 - p4)
-    BBBAA <- p1 + p2 + p3 + (1 - p4)
-    BAABA <- p1 + (1 - p2) + (1 - p3) + p4
-    AAABA <- (1 - p1) + (1 - p2) + (1 - p3) + p4
+    ABBAA <- (1 - p1) * p2 * p3 * (1 - p4)
+    BBBAA <- p1 * p2 * p3 * (1 - p4)
+    BAABA <- p1 * (1 - p2) * (1 - p3) * p4
+    AAABA <- (1 - p1) * (1 - p2) * (1 - p3) * p4
 
-    ABABA <- (1 - p1) + p2 + (1 - p3) + p4
-    BBABA <- p1 + p2 + (1 - p3) + p4
-    BABAA <- p1 + (1 - p2) + p3 + (1 - p4)
-    AABAA <- (1 - p1) + (1 - p2) + p3 + (1 - p4)
+    ABABA <- (1 - p1) * p2 * (1 - p3) * p4
+    BBABA <- p1 * p2 * (1 - p3) * p4
+    BABAA <- p1 * (1 - p2) * p3 * (1 - p4)
+    AABAA <- (1 - p1) * (1 - p2) * p3 * (1 - p4)
 
     first_il <- (sum(ABBAA) + sum(BBBAA) + sum(BAABA) + sum(AAABA))
     second_il <- (sum(ABABA) + sum(BBABA) + sum(BABAA) + sum(AABAA))
@@ -39,15 +39,15 @@ Dil.stat <- function(p1, p2, p3, p4) {
 
 Dfi.stat <- function(p1, p2, p3, p4) {
 
-    BABAA <- p1 + (1 - p2) + p3 + (1 - p4)
-    BABBA <- p1 + (1 - p2) + p3 + p4
-    ABABA <- (1 - p1) + p2 + (1 - p3) + p4
-    ABAAA <- (1 - p1) + p2 + (1 - p3) + (1 - p4)
+    BABAA <- p1 * (1 - p2) * p3 * (1 - p4)
+    BABBA <- p1 * (1 - p2) * p3 * p4
+    ABABA <- (1 - p1) * p2 * (1 - p3) * p4
+    ABAAA <- (1 - p1) * p2 * (1 - p3) * (1 - p4)
 
-    ABBAA <- (1 - p1) + p2 + p3 + (1 - p4)
-    ABBBA <- (1 - p1) + p2 + p3 + p4
-    BAABA <- p1 + (1 - p2) + (1 - p3) + p4
-    BAAAA <- p1 + (1 - p2) + (1 - p3) + (1 - p4)
+    ABBAA <- (1 - p1) * p2 * p3 * (1 - p4)
+    ABBBA <- (1 - p1) * p2 * p3 * p4
+    BAABA <- p1 * (1 - p2) * (1 - p3) * p4
+    BAAAA <- p1 * (1 - p2) * (1 - p3) * (1 - p4)
 
     first_fi <- (sum(BABAA) + sum(BABBA) + sum(ABABA) + sum(ABAAA))
     second_fi <- (sum(ABBAA) + sum(ABBBA) + sum(BAABA) + sum(BAAAA))
@@ -57,15 +57,15 @@ Dfi.stat <- function(p1, p2, p3, p4) {
 
 Dol.stat <- function(p1, p2, p3, p4) {
 
-    BAABA <- p1 + (1 - p2) + (1 - p3) + p4
-    BABBA <- p1 + (1 - p2) + p3 + p4
-    ABBAA <- (1 - p1) + p2 + p3 + (1 - p4)
-    ABAAA <- (1 - p1) + p2 + (1 - p3) + (1 - p4)
+    BAABA <- p1 * (1 - p2) * (1 - p3) * p4
+    BABBA <- p1 * (1 - p2) * p3 * p4
+    ABBAA <- (1 - p1) * p2 * p3 * (1 - p4)
+    ABAAA <- (1 - p1) * p2 * (1 - p3) * (1 - p4)
 
-    ABABA <- (1 - p1) + p2 + (1 - p3) + p4
-    ABBBA <- (1 - p1) + p2 + p3 + p4
-    BABAA <- p1 + (1 - p2) + p3 + (1 - p4)
-    BAAAA <- p1 + (1 - p2) + (1 - p3) + (1 - p4)
+    ABABA <- (1 - p1) * p2 * (1 - p3) * p4
+    ABBBA <- (1 - p1) * p2 * p3 * p4
+    BABAA <- p1 * (1 - p2) * p3 * (1 - p4)
+    BAAAA <- p1 * (1 - p2) * (1 - p3) * (1 - p4)
 
     first_ol <- (sum(BAABA) + sum(BABBA) + sum(ABBAA) + sum(ABAAA))
     second_ol <- (sum(ABABA) + sum(ABBBA) + sum(BABAA) + sum(BAAAA))
@@ -74,7 +74,7 @@ Dol.stat <- function(p1, p2, p3, p4) {
     }
 
 
-freq_table = read.table("BfBfPePeRf.geno.tsv", header=T, as.is=T)
+freq_table = read.table("PePeBfBfRf.geno.tsv", header=T, as.is=T)
 
 #removing NA values
 freq_table2<-freq_table
@@ -86,10 +86,16 @@ Dfi <- Dfi.stat(freq_table[,"P1"], freq_table[,"P2"], freq_table[,"P3"], freq_ta
 Dol <- Dol.stat(freq_table[,"P1"], freq_table[,"P2"], freq_table[,"P3"], freq_table[,"P4"])
 
 
-Dfo_X2 <- (first_fo - second_fo)^2 / (first_fo + second_fo)
-Dil_X2 <- (first_il - second_il)^2 / (first_il + second_il)
-Dfi_X2 <- (first_fi - second_fi)^2 / (first_fi + second_fi)
-Dol_X2 <- (first_ol - second_ol)^2 / (first_ol + second_ol)
+Dfo
+Dil
+Dfi
+Dol
+
+cat(paste("Dfo =", round(Dfo,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dil =", round(Dil,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dfi =", round(Dfi,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dol =", round(Dol,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+
 
 source("~/genomics_general-master/jackknife.R")
 
@@ -99,7 +105,8 @@ block_indices <- get_block_indices(block_size=1e6,
 
 n_blocks <- length(block_indices)
 
-n_blocks
+cat(paste("Genome divided into", n_blocks, "blocks."),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+
 
 Dfo_sd <- get_jackknife_sd(block_indices=block_indices,
                          FUN=Dfo.stat,
@@ -110,6 +117,10 @@ Dfo_err <- Dfo_sd/sqrt(n_blocks)
 Dfo_Z <- Dfo / Dfo_err
 Dfo_p <- 2*pnorm(-abs(Dfo_Z))
 
+cat(paste("Dfo_sd =", round(Dfo_sd,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dil_err =", round(Dil_err,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dfo_Z =", round(Dfo_Z,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dfo_p =", round(Dfo_p,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
 
 
 
@@ -123,6 +134,11 @@ Dil_err <- Dil_sd/sqrt(n_blocks)
 Dil_Z <- Dil / Dil_err
 Dil_p <- 2*pnorm(-abs(Dil_Z))
 
+cat(paste("Dil_sd =", round(Dil_sd,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dil_err =", round(Dil_err,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dil_Z =", round(Dil_Z,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dil_p =", round(Dil_p,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+
 
 
 Dfi_sd <- get_jackknife_sd(block_indices=block_indices,
@@ -135,6 +151,10 @@ Dfi_err <- Dfi_sd/sqrt(n_blocks)
 Dfi_Z <- Dfi / Dfi_err
 Dfi_p <- 2*pnorm(-abs(Dfi_Z))
 
+cat(paste("Dfi_sd =", round(Dfi_sd,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dfi_err =", round(Dfi_err,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dfi_Z =", round(Dfi_Z,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dfi_p =", round(Dfi_p,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
 
 
 
@@ -147,50 +167,26 @@ Dol_err <- Dol_sd/sqrt(n_blocks)
 Dol_Z <- Dol / Dol_err
 Dol_p <- 2*pnorm(-abs(Dol_Z))
 
+cat(paste("Dol_sd =", round(Dol_sd,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dol_err =", round(Dol_err,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dol_Z =", round(Dol_Z,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
+cat(paste("Dol_p =", round(Dol_p,4)),file=paste0("5taxon.abbawholegenome.stats.txt"),sep="\n",append=TRUE)
 
 
 
-Dfo_err
-Dfo_Z
-Dfo_p
 
-Dil_err
-Dil_Z
-Dil_p
 
-Dfi_err
-Dfi_Z
-Dfi_p
 
-Dol_err
-Dol_Z
-Dol_p
 
-cat(paste("D =", round(D,4)),file=paste0(outputdirectory,"/",project_name,".abbawholegenome.stats.txt"),sep="\n",append=TRUE)
 
-source(paste0(simonhmartin_directory,"/jackknife.R"))
 
-block_indices <- get_block_indices(block_size=1e6,
-                                   positions=freq_table$position,
-                                   chromosomes=freq_table$scaffold)
 
-n_blocks <- length(block_indices)
 
-cat(paste("Genome divided into", n_blocks, "blocks."),file=paste0(outputdirectory,"/",project_name,".abbawholegenome.stats.txt"),sep="\n",append=TRUE)
 
-D_sd <- get_jackknife_sd(block_indices=block_indices,
-                         FUN=D.stat,
-                         freq_table[,"P1"], freq_table[,"P2"], freq_table[,"P3"])
 
-cat(paste("D standard deviation = ", round(D_sd,4)),file=paste0(outputdirectory,"/",project_name,".abbawholegenome.stats.txt"),sep="\n",append=TRUE)
 
-D_err <- D_sd/sqrt(n_blocks)
-D_Z <- D / D_err
-D_p <- 2*pnorm(-abs(D_Z))
 
-cat(paste("D Z score = ", round(D_Z,3)),file=paste0(outputdirectory,"/",project_name,".abbawholegenome.stats.txt"),sep="\n",append=TRUE)
 
-cat(paste("D p value = ", round(D_p,3)),file=paste0(outputdirectory,"/",project_name,".abbawholegenome.stats.txt"),sep="\n",append=TRUE)
 
 
 
