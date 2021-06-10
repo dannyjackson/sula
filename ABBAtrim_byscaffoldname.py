5c0e4c704f1683ec481af7a2c3aa376cd20ef964
Danny Jackson <dnjacks4@asu.edu>
2021-06-09 19:32:29 -0700

    Update ABBAtrim_byscaffoldname.py

--- ABBAtrim_byscaffoldname.py
+++ ABBAtrim_byscaffoldname.py
@@ -2,9 +2,9 @@ import sys
 import numpy as np
 import pandas as pd
 
-mask = pd.read_csv(sys.argv[0], sep='\t', names="a")
+mask = pd.read_csv(sys.argv[1], sep='\t', names="a")
 
-df = pd.read_csv(sys.argv[1], sep='\t')
+df = pd.read_csv(sys.argv[2], sep='\t')
 
 filtered = df[df['scaffold'].isin(mask['a'])]
 
